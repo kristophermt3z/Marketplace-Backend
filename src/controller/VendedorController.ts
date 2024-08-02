5de8bf6c93231ac79aeef8bf0cdbf4e900748f7c
salvador <salvador.martinez.ksmg@gmail.com>
2024-08-02 13:58:46 -0600

    login and register finished

--- src/controller/VendedorController.ts
+++ src/controller/VendedorController.ts
@@ -5,63 +5,65 @@ import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const createVendedor = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if (!(email && password)) {
-        res.status(400).send("Todos los campos son necesarios (email y password).");
-        return;
-    }
-    console.log('first')
-    const vendedorRepository = AppDataSource.getRepository(vendedores);
+  if (!(email && password)) {
+    res.status(404).send("All fields are required (email and password).");
+    return;
+  }
+  const vendedorRepository = AppDataSource.getRepository(vendedores);
 
-    const existingVendedor = await vendedorRepository.findOneBy({ email });
-    if (existingVendedor) {
-        res.status(409).send("Un vendedor con este email ya existe.");
-        return;
-    }
+  const existingVendedor = await vendedorRepository.findOneBy({ email });
+  if (existingVendedor) {
+    res.status(404).send("A seller with this email already exists.");
+    return;
+  }
 
-    try {
-        const vendedor = new vendedores();
-        vendedor.email = email;
-        vendedor.password = await bcryptjs.hash(password, 10);
-        vendedor.role = "vendedor";
+  try {
+    const vendedor = new vendedores();
+    vendedor.email = email;
+    vendedor.password = await bcryptjs.hash(password, 10);
+    vendedor.role = "vendedor";
 
-        await vendedorRepository.save(vendedor);
-        res.status(201).send("Vendedor creado exitosamente.");
-    } catch (error) {
-        console.error("Error al crear el vendedor:", error);
-        res.status(500).send("Error interno del servidor.");
-    }
+    await vendedorRepository.save(vendedor);
+    res.status(201).send("Seller created successfully.");
+  } catch (error) {
+    console.error("Error creating seller:", error);
+    res.status(500).send("Internal server error.");
+  }
 };
 
 export const loginVendedor = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
-    console.log('second')
-  
-    if (!(email && password)) {
-      res.status(400).send("Both email and password are required.");
-      return;
-    }
-  
-    const vendedorRepository = AppDataSource.getRepository(vendedores);
-    const vendedor = await vendedorRepository.findOneBy({ email });
-  
-    if (!vendedor) {
-      res.status(404).send("User not found.");
-      return;
-    }
-  
-    const isPasswordValid = await bcryptjs.compare(password, vendedor.password);
-  
-    if (!isPasswordValid) {
-      res.status(401).send("Incorrect password.");
-      return;
-    }
-  
-    // Create a token
-    const token = jwt.sign({ id: vendedor.id, role: vendedor.role }, process.env.JWT_SECRET as string, {
+  const { email, password } = req.body;
+
+  if (!(email && password)) {
+    res.status(404).send("Both email and password are required.");
+    return;
+  }
+
+  const vendedorRepository = AppDataSource.getRepository(vendedores);
+  const vendedor = await vendedorRepository.findOneBy({ email });
+
+  if (!vendedor) {
+    res.status(404).send("User not found.");
+    return;
+  }
+
+  const isPasswordValid = await bcryptjs.compare(password, vendedor.password);
+
+  if (!isPasswordValid) {
+    res.status(404).send("Incorrect password.");
+    return;
+  }
+
+  // Create a token
+  const token = jwt.sign(
+    { id: vendedor.id, role: vendedor.role },
+    process.env.JWT_SECRET as string,
+    {
       expiresIn: "1h",
-    });
-  
-    res.json({ message: "Login successful", token });
-  };
+    }
+  );
+
+  res.json({ message: "Login successful", token });
+};
